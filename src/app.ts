d3215d89ba58a1544a39b21191057aff3596ce67
Rupert Foggo McKay <maxfmckay@gmail.com>
2025-01-17 12:40:31 +0100

    fen parsing

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,55 @@
-import { Chess } from "chess.js";
+import { Chess, parseSquare, type Move, fen } from "chessops";
 import { Chessground } from "chessground";
 
+const toPlayDisplay = document.querySelector<HTMLSpanElement>("#to-play")!;
+
+const fenForm = document.querySelector<HTMLFormElement>("#fen-form")!;
+const fenInput = document.querySelector<HTMLInputElement>("#fen")!;
+
 // Stateful chess
-const chess = new Chess();
+let chess = Chess.default();
 
 // Chess UI
-const board = Chessground(document.getElementById("board")!, {
+const board = Chessground(document.querySelector<HTMLDivElement>("#board")!, {
+  highlight: {
+    lastMove: false,
+  },
   movable: {
     events: {
       after: (from, to) => {
-        try {
-          // An illegal move throws an error
-          chess.move({ from, to, promotion: "q" });
-        } catch (error) {
-          console.error(error);
-        } finally {
-          // Either chess was legally updated or not,
-          // either way this updates the UI accordingly
-          board.set({ fen: chess.fen() });
-        }
+        const move: Move = {
+          from: parseSquare(from)!,
+          to: parseSquare(to)!,
+        };
+
+        if (chess.isLegal(move)) chess.play(move);
+        // This is a hack to force queen promotion
+        // We can't add it in the previous 'if', because then it would be treated as an illegal move
+        else if (chess.isLegal({ ...move, promotion: "queen" }))
+          chess.play({ ...move, promotion: "queen" });
+        board.set({ fen: fen.makeBoardFen(chess.board) });
+        toPlayDisplay.textContent = `${chess.turn} to play`;
       },
     },
   },
+});
+
+fenForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const pgnInputValue = fenInput.value;
+  if (!pgnInputValue) return;
+
+  try {
+    const setup = fen.parseFen(fenInput.value).unwrap();
+    const pos = Chess.fromSetup(setup).unwrap();
+    chess = pos;
+
+    board.set({ fen: fen.makeBoardFen(chess.board) });
+    toPlayDisplay.textContent = `${chess.turn} to play`;
+  } catch (error) {
+    console.error(error);
+  }
+
+  // Clear the input after submission
+  fenInput.value = "";
 });
